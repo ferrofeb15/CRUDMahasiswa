f146daafcc1020a165750e6487bc37b2a30c633d
ferrofeb15 <ferro.febrianto1531@gmail.com>
2020-05-14 11:07:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/crudmahasiswa/Konfigurasi.java
@@ -0,0 +1,22 @@
+package com.example.crudmahasiswa;
+
+public class Konfigurasi {
+    public static final String URL_ADD="http://192.168.43.14/cob/insert.php";
+    public static final String URL_GET_ALL="http://192.168.43.14/cob/read.php";
+    public static final String URL_GET_MHS="http://192.168.43.14/cob/select.php?id=";
+    public static final String URL_UPDATE_MHS="http://192.168.43.14/cob/update.php";
+    public static final String URL_DELETE_MHS="http://192.168.43.14/cob/delete.php?id=";
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+
+    public static final String MHS_ID = "mhs_id";
+}
